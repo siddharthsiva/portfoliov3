814021e935c9544359bb0908011c95339bf12dd5
Siddharth Sivalanka <siddharthsivalanka@gmail.com>
2026-03-01 20:53:04 -0800

    fix base

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,6 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/portfoliov3/",
   server: {
     host: "::",
     port: 8080,
